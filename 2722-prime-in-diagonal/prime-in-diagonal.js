db2619c0b616f0ad526912e6a73f7690211bb906
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2023-07-15 22:46:50 -0500

    Time: 193 ms (22.22%) | Memory: 49.9 MB (56.86%) - LeetSync

---
+++ 2722-prime-in-diagonal/prime-in-diagonal.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} nums
+ * @return {number}
+ */
+function diagonalPrime(nums) {
+    function isPrime(num){
+        if(num <= 1) return false;
+        for(let i = 2; i<num; i++){
+            if(num % i == 0){
+            return false;
+            break;
+            }
+        }
+        return true;
+    }
+
+    let primeArr = [];
+    let len = nums.length-1;
+    let res = 0;
+
+    for(let i = 0; i <= len; i++){
+        let num1 = nums[i][i];
+        let num2 = nums[i][len - i];
+        if(num1 > res && isPrime(num1)){
+            res = num1;
+        };
+        if(num2 > res && isPrime(num2)){
+            res = num2;
+        };
+    }
+    return res;
+};
+
