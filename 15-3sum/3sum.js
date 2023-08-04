97697e4d3c0b963b3945a4ded0216970dc4fcda1
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2023-08-04 14:38:47 -0500

    Time: 142 ms (98.34%) | Memory: 59.2 MB (51.92%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -19,9 +19,9 @@ function threeSum (nums) {
             if((nums[i] + nums[l] + nums[r]) == 0){
                 resArr.push([nums[i], nums[l], nums[r]]);
                 while(l < r && nums[l] == nums[l+1])l++;
-                while(l < r && nums[r] == nums[r-1])r--;
+                //while(l < r && nums[r] == nums[r-1])r--;
                 l++;
-                r--;
+                //r--;
             } else if((nums[i] + nums[l] + nums[r]) > 0){
                 r--;
             } else if((nums[i] + nums[l] + nums[r]) < 0) {
