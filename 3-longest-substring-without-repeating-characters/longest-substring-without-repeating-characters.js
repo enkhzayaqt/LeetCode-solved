709349cca543db33e32a34d71552c8cdbe00453e
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2023-09-02 22:08:49 -0500

    Time: 235 ms (19.81%) | Memory: 49.1 MB (22.96%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,19 +2,29 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+function lengthOfLongestSubstring (s) {
+    if(s.length == 1) return 1;
+    // create new map
     let map = new Map();
+    // initiate max len 0
     let max = 0;
+    // outer loop for starting character
+    // inner loop to find out max len
     for(let i = 0; i < s.length; i++){
         for(let j = i; j < s.length; j++){
+            // check if map has char
+            // if map has it break and clear the map
+            // else add it to map
             if(!map.has(s[j])){
                 map.set(s[j]);
-                max = Math.max(max, map.size);
             } else {
+                max = Math.max(max, map.size);
                 map.clear();
-                break;
+                break
             }
         }
+        // update max len by using Math.max
+        // return max len
     }
     return max;
 };
