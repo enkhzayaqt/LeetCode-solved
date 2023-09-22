d035bba00d2e8f6f7f64369276c08c9644b707d0
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2023-09-21 21:21:02 -0500

    Time: 51 ms (85.72%) | Memory: 42.5 MB (57.86%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.js
+++ 152-maximum-product-subarray/maximum-product-subarray.js
@@ -3,16 +3,26 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let max = Math.max(...nums);
-
-    for(let i = 0; i < nums.length; i++){   
-        let temp = nums[i];     
-        
-        for(let j= i+1; j < nums.length; j++){
-            temp *= nums[j];
-            max = Math.max(max, temp)
-        }
-
+  let max = Math.max(...nums);
+  let max2 = -Infinity;
+  let sum = 1;
+  for (let i = 0; i < nums.length; i++) {
+    sum *= nums[i];
+    max = Math.max(max, sum);
+    if (sum == 0) {
+      sum = 1;
     }
-    return max;
+  }
+  sum = 1;
+
+  for (let i = nums.length - 1; i > 0; i--) {
+    sum *= nums[i];
+    max2 = Math.max(max2, sum);
+    if (sum == 0) {
+      sum = 1;
+    }
+  }
+  let res = Math.max(max, max2);
+
+  return res;
 };
