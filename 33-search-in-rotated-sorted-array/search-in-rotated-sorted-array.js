df02c1d78066c9694751e7ca8e01b103d8ce0ec0
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2023-09-22 01:48:02 -0500

    Time: 55 ms (40.01%) | Memory: 41.5 MB (88.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,5 +4,23 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target)
+    let lo = 0, hi = nums.length -1;
+    while(lo <= hi) {
+        let mid = Math.floor((lo+hi)/2);
+
+        if(nums[mid] == target) return mid;
+        
+        // if left side is sorted
+        if(nums[lo] <= nums[mid]){
+            if(nums[lo] <= target && target < nums[mid]){
+                hi = mid-1;
+            }else lo = mid +1 
+        } else {
+            if(nums[mid] < target && target <= nums[hi]){
+                lo = mid +1;
+            }else hi = mid-1;
+        }
+        
+    }
+    return -1
 };
