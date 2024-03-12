5457c6f8365dd1c19ce642586f394e550bf80675
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-03-12 17:26:22 -0500

    Time: 161 ms (57.36%) | Memory: 65.5 MB (48.26%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,30 +2,29 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-function threeSum (nums) {
-    const resArr = [];
-    if(nums.length < 3) return resArr;
+var threeSum = function(nums) {
+    nums.sort((a,b)=>a-b)
+    let res = [];
 
-    nums = nums.sort((a,b)=>a-b);
+    for(let i = 0; i< nums.length-2; i++){
+        if(i>0 && nums[i] == nums[i-1]) continue
 
-    for(let i = 0; i < nums.length-2; i++){
-        if(nums[i] > 0) break;
-        if(i>0 && nums[i] == nums[i-1]) continue;
-
-        let l = i + 1;
-        let r = nums.length - 1;
-
-        while(l < r){
-            if((nums[i] + nums[l] + nums[r]) == 0){
-                resArr.push([nums[i], nums[l], nums[r]]);
-                while(l < r && nums[r] == nums[r-1])r--;
-                r--;
-            } else if((nums[i] + nums[l] + nums[r]) > 0){
-                r--;
-            } else if((nums[i] + nums[l] + nums[r]) < 0) {
-                l++;
-            }
+        let l = i+1;
+        let r = nums.length-1;
+        while(l<r){
+            let sum = (nums[i] + nums[l] + nums[r]);
+            if(sum<0){
+                l+=1;
+            }else if(sum>0){
+                r-=1;
+            }else {
+                res.push([nums[i], nums[l], nums[r]]);
+                l+=1;
+                while(nums[l] == nums[l-1] && l<r){
+                    l += 1;
+                } 
+            }           
         }
     }
-    return resArr;
+    return res;
 };
