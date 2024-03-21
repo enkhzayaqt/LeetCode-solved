7758d4b645beb733603b117900b38f4cff23c970
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-03-21 10:14:55 -0500

    Time: 58 ms (26.66%) | Memory: 48.3 MB (94.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,23 +4,31 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let lo = 0, hi = nums.length -1;
-    while(lo <= hi) {
-        let mid = Math.floor((lo+hi)/2);
-
-        if(nums[mid] == target) return mid;
-        
-        // if left side is sorted
-        if(nums[lo] <= nums[mid]){
-            if(nums[lo] <= target && target < nums[mid]){
-                hi = mid-1;
-            }else lo = mid +1 
-        } else {
-            if(nums[mid] < target && target <= nums[hi]){
-                lo = mid +1;
-            }else hi = mid-1;
+    // use binary search time: O(log n)
+    // set l, r, m pointer
+    // while l < r check m 
+    // if m is target return m
+    // if m is greater than target 
+    // & l is also greater then move l to m
+    // if m is less than target & l is greater than target move l to m
+    let l = 0;
+    let r = nums.length - 1;
+    while(l <= r){
+        let m = Math.floor((l+r) / 2 );
+        if(nums[m] == target) return m;
+        if(nums[l] <= nums[m] ){
+            if(target > nums[m] || target < nums[l]){
+                l = m + 1;
+            }else {
+                r = m - 1;
+            }
+        }else {
+            if(target < nums[m] || target > nums[r]){
+                r = m - 1;
+            }else {
+                l = m + 1;
+            }
         }
-        
     }
-    return -1
+    return -1;
 };
