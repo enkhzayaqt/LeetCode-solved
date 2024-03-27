8cc166f0c5f2fc019144aea115e9650af96b04f0
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-03-27 11:06:53 -0500

    Time: 1244 ms (9.43%) | Memory: 55.1 MB (23.72%) - LeetSync

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ const productOfArray = (arr)=>{
+
+ }
+var numSubarrayProductLessThanK = function(nums, k) {
+    // find all the sub array using nested loop
+    // check if product is less than k increase count
+    // return count
+    let count = 0;
+    for(let i = 0 ; i < nums.length; i++){
+        let product = 1;
+        for(let j = i ; j < nums.length; j++){
+            product = product * nums[j]
+            if(product >= k) break
+            count++;
+        }
+    }
+    return count;
+};
