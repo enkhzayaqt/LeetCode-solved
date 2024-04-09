e2fd00d76e32fb86a7fb493bdba4484ec46fff8e
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-04-08 23:37:33 -0500

    Time: 53 ms (66.12%) | Memory: 48.3 MB (94.54%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    // let timer = 0;
+    // let i = 0;
+    // while(tickets[k] !== 0){
+    //     ti 
+    //     i++;
+    //     timer++;
+    // }
+    // return timer;
+    let timer = 0;
+    for(let i = 0; i < tickets.length; i++){
+        if(i <= k){
+            if(tickets[i] <= tickets[k]){
+                timer += tickets[i]
+            }else {
+                timer += tickets[k]
+            }
+        }else {
+            if(tickets[i] <= tickets[k] - 1){
+                timer += tickets[i]
+            }else {
+                timer += tickets[k] - 1; 
+            }
+        }
+    }
+    return timer;
+};
