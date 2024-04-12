318c0ee2781e7d8c534723014e94b2028b37f206
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-04-11 19:08:19 -0500

    Time: 70 ms (39.81%) | Memory: 56.1 MB (45.63%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    // edge case:
+    if(num.length == k) return '0';
+    let stack = [];
+    for(let n of num){
+        while(k > 0 && stack.length && stack[stack.length-1] > n){
+            k--;
+            stack.pop();
+        }
+        if(stack.length == 0 && n == '0') continue
+        stack.push(n);
+    }
+    while(k>0 && stack.length>0){
+        k--;
+        stack.pop();
+    }
+    return stack.length == 0 ? '0' : stack.join('');
+};
