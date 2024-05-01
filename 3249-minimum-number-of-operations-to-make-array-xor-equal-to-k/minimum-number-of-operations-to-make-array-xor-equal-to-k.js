61d4331933b4939e44be0fe76d89967915cbf08d
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-05-01 11:25:44 -0500

    Time: 79 ms (63.82%) | Memory: 59.7 MB (91.94%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.js
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var minOperations = function(nums, k) {
-    // step 1: find XOR of nums array;
+    // step 1: calculates the XOR of all elements in the nums array using the reduce method;
     let xor = nums.reduce((acc,cur)=> acc ^ cur, 0);
 
     // step 2: compare XOR result with target K;
@@ -14,7 +14,7 @@ var minOperations = function(nums, k) {
     let count = 0;
     while(temp > 0){
         count += temp & 1;
-        temp >>= 1;
+        temp >>= 1;  // shifts temp to the right by 1 bit to check the next bit
     }
     return count; 
     // time: O(n);
