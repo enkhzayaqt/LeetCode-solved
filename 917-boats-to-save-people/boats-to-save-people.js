5deb307f8ccf1fcdbb2ebf6582abf524e6ce1f97
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-05-04 13:38:56 -0500

    Time: 132 ms (41.74%) | Memory: 57.3 MB (26.96%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people.sort((a,b) => a-b);
+    let boats = 0;
+    let l = 0;
+    let r = people.length-1;
+
+    while(l<=r){
+        if(people[l] + people[r] <= limit){
+            l++;
+            r--;
+        }else r--;
+
+        boats++;
+    }
+    return boats;
+};
