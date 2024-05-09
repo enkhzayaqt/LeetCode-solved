3c0f009925d130918549365e8483664b5cc8e398
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-05-09 10:41:23 -0500

    Time: 253 ms (27.85%) | Memory: 67.2 MB (71.23%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
@@ -6,7 +6,6 @@
 var maximumHappinessSum = function(happiness, k) {
     // sort the array;
     // pick k number of max kids and before saving them into maxArr reduce by i times;
-    
     let i = 1; 
     let sorted = happiness.sort((a,b) => b-a);
     let max = happiness[0];
