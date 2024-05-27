73434df92697d6bff12b12ec83529a88b6006ff2
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-05-27 13:14:46 -0500

    Time: 54 ms (62.96%) | Memory: 49.3 MB (50.93%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.js
@@ -5,12 +5,11 @@
 var specialArray = function(nums) {
     nums.sort((a,b) => b-a);
     let poped = [];
+
     for(let i = nums.length-1; i >= 0; i--){
-        if(nums[i] < nums.length){
-            poped.push(nums.pop());
-        }else if(nums[i] >= nums.length && !poped.includes(nums.length)){
-            return nums.length;
-        }
+        if(nums[i] < nums.length) poped.push(nums.pop());
+        if(nums[i] >= nums.length && !poped.includes(nums.length)) return nums.length;
     }
+    
     return -1
 };
