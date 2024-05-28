0dc6910c96cbdfbbfe9b8060d8252f5b81b82a50
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-05-27 21:41:16 -0500

    Time: 48 ms (84.24%) | Memory: 48.5 MB (64.88%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -2,7 +2,10 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fib = function(n, memo = {}) {
     if(n < 2) return n;
-    return fib(n-1) + fib(n-2);
+    if(n in memo) return memo[n];
+
+    memo[n] = fib(n-1, memo) + fib(n-2, memo);
+    return memo[n];
 };
