bd44a652ab700572b0ea926f0107da56faf1fb72
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-06-01 12:56:58 -0500

    Time: 77 ms (7.06%) | Memory: 49.2 MB (94.11%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    let arr = [];
     let sum = 0;
-    for(let i in s){
-        arr.push(s.charCodeAt(i))
+    for(let i = 0; i < s.length-1; i++ ){
+        let a = s.charCodeAt(i);
+        let b = s.charCodeAt(i+1);
+        sum += Math.abs(a-b);
     };
-    for(let i = 0; i < s.length-1; i++){
-        sum += (Math.abs(arr[i]- arr[i+1]))
-    }
+    // for(let i = 0; i < s.length-1; i++){
+    //     sum += (Math.abs(arr[i]- arr[i+1]))
+    // }
     return sum;
 };
