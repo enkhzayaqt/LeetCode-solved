5c7ce64a93785706ccdf423775547407222d6471
Enkhzaya Munkhochir <82678150+enkhzayaqt@users.noreply.github.com>
2024-08-31 09:31:24 -0500

    Time: 55 ms (82.99%) | Memory: 51.1 MB (39.68%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let obj = {};
-    for(let i = 0 ; i< nums.length; i++){
-        obj[nums[i]] = i;
-    }
-    for(let i in nums){
-        if(obj[target - nums[i]]){
-            if(i == obj[target - nums[i]]) continue;
-            return [i, obj[target - nums[i]]]
+    const obj = {};
+    for(let i = 0; i< nums.length; i++){
+        let diff = target - nums[i];
+        if(diff in obj){
+            return [i, obj[diff]]
+        }else {
+            obj[nums[i]] = i;
         }
     }
+    return [];
 };
